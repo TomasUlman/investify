01e0bc2e46484f2a54f52871db8a7078903b3062
Tomáš Ulman <130840571+TomasUlman@users.noreply.github.com>
2025-02-25 13:46:07 +0100

    Updated responsive styles

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './styles.css';
-import './sm-styles.css';
+import './sm-styles-v2.css';
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
